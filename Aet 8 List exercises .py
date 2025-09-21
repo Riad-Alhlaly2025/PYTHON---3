0ee3d2d307a34b84a99c50e9782ca84c1bd0745f
Riadh Alhlaly <ryadsalhalhlaly@gmail.com>
2025-09-21 21:41:09 +0300

    Aet 8 List exercises .py

--- Aet 8 List exercises .py
+++ Aet 8 List exercises .py
@@ -3,9 +3,11 @@
 # واذا كتبت له لا يعني الشخص غير موجود بيطبع لم يتم التاكيد
 
 name=["Riad", "Ali", "Saleh"]
+# التحقق من الاسماء كلها واحد واحد
 for num in name:
     print()
     print(num)
+    # هل المستخدم التالي موجود اكتب نعم
     nambr=input("Is this person attending ? (yas or no): ").lower()
     
     if nambr=="yas":
